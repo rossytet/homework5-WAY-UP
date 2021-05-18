483b0ca87f13b87b73cee98a01e72fdbb4711054
Ilya <83479109+rossytet@users.noreply.github.com>
2021-05-18 15:56:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#way .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+
+
+	$(window).scroll(function() {
+	    $('#skills .skill-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+	
+})
